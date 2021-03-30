d8e26f2bf63679ad0e40e34e2f51402e902bc012
Jarrett Meyer <meyer_jarrett@lilly.com>
2021-03-30 06:09:19 -0400

    updates

---
+++ README.md
@@ -0,0 +1,28 @@
+# github-action-package-values
+
+Gets commit and package values for your Node project.
+
+## Usage
+
+```yaml
+jobs:
+  runs_on: ubuntu-latest
+  steps:
+    - name: Checkout latest
+      uses: actions/checkout@v2
+    - name: Get package values
+      uses: jarrettmeyer/package-values
+# snip
+```
+
+## Output vars
+
+| var              | description                                                       |
+| :--------------- | :---------------------------------------------------------------- |
+| commit_sha       | The commit sha, e.g. `ecca45978ef4d50f5b7042f898ddc383f89b6ead`   |
+| commit_sha_short | The short commit sha, e.g. `ecca459`                              |
+| commit_author    | The commit author, e.g. `Jarrett Meyer <meyer_jarrett@lilly.com>` |
+| commit_date      | The commit date, e.g. `Tue Mar 30 05:28:25 2021 -0400`            |
+| commit_timestamp | The commit date as UNIX timestamp, e.g. `1617096505000`           |
+| package_name     | The `package.json` name, e.g. `github-action-package-values`      |
+| package_version  | The `package.json` version, e.g. `1.0.0`                          |
--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 import core from "@actions/core";
 import { execSync } from "child_process";
+import { EOL } from "os";
 import { resolve } from "path";
 
-const commitSha = execSync("git rev-parse HEAD");
-const commitShaShort = execSync("git rev-parse --short HEAD");
+const commitSha = execSync("git rev-parse HEAD").toString("utf-8");
+const commitShaShort = execSync("git rev-parse --short HEAD").toString("utf-8");
 
-core.setOutput("commit_sha", commitSha)
+core.setOutput("commit_sha", commitSha);
 core.setOutput("commit_sha_short", commitShaShort);
+
+const commitLog = execSync("git log -1").toString("utf-8").split(EOL);
+const commitAuthor = commitLog[1].substring(8);
+const commitDate = commitLog[2].substring(8);
+const commitTimestamp = new Date(commitDate).valueOf();
+
+core.setOutput("commit_author", commitAuthor);
+core.setOutput("commit_date", commitDate);
+core.setOutput("commit_timestamp", commitTimestamp);
 
 const basePath = core.getInput("path");
 const packagePath = resolve(basePath, "package.json");
 const package = require(packagePath);
 
 if (package) {
-  if (package.name) {
+  if (typeof package.name === "string") {
     core.setOutput("package_name");
   }
-  if (package.version) {
+  if (typeof package.version === "string") {
     core.setOutput("package_version");
   }
 }
-
-
-
