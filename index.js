b7d0fb9ad62b799ca6cf91148e8a48bf4cb5c225
Jarrett Meyer <meyer_jarrett@lilly.com>
2021-03-30 06:48:54 -0400

    change import to require

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-import core from "@actions/core";
-import { execSync } from "child_process";
-import { EOL } from "os";
-import { resolve } from "path";
+const core = require("@actions/core");
+const cp = require("child_process");
+const path = require("path");
 
-const commitSha = execSync("git rev-parse HEAD").toString("utf-8");
-const commitShaShort = execSync("git rev-parse --short HEAD").toString("utf-8");
+const commitSha = cp.execSync("git rev-parse HEAD").toString("utf-8");
+const commitShaShort = cp
+  .execSync("git rev-parse --short HEAD")
+  .toString("utf-8");
 
 core.setOutput("commit_sha", commitSha);
 core.setOutput("commit_sha_short", commitShaShort);
 
-const commitLog = execSync("git log -1").toString("utf-8").split(EOL);
+const commitLog = execSync("git log -1").toString("utf-8").split("\n");
 const commitAuthor = commitLog[1].substring(8);
 const commitDate = commitLog[2].substring(8);
 const commitTimestamp = new Date(commitDate).valueOf();
@@ -19,7 +20,7 @@ core.setOutput("commit_date", commitDate);
 core.setOutput("commit_timestamp", commitTimestamp);
 
 const basePath = core.getInput("path");
-const packagePath = resolve(basePath, "package.json");
+const packagePath = path.resolve(basePath, "package.json");
 const package = require(packagePath);
 
 if (package) {
